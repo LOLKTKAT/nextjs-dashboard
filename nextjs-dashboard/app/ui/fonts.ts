5ab97aac325963dec2458048a4bd43ee2e08ceaa
LOLKTKAT <88342619+LOLKTKAT@users.noreply.github.com>
2024-01-15 20:29:31 +0300

    removed everything

--- nextjs-dashboard/app/ui/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter, Space_Grotesk, Lustria, Lusitana } from 'next/font/google';
-
-export const inter = Inter({ subsets: ['latin'] });
-export const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
-export const lusitana = Lusitana({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-});

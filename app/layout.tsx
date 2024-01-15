1d0e3f72874cb9c28308717da7022111a765bd97
LOLKTKAT <88342619+LOLKTKAT@users.noreply.github.com>
2024-01-15 20:43:01 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import { spaceGrotesk } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={spaceGrotesk.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Space_Grotesk, Lustria, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
